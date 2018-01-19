ce6dac85dbac55e635416e0e8459ae1c9f9296bf
wolfhound <v.maslov@ihc.ru>
2018-01-19 16:52:00 +0500

    Initial Commit

---
+++ zavbusTest1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zavbusTest1
+//
+//  Created by Vladimir Maslov on 19.01.2018.
+//  Copyright © 2018 Vladimir Maslov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
